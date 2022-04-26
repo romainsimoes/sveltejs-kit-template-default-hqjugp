86ab74ef5158997dbc7bda8f4b8905163d079a0e
romainsimoes <romain.simoes.pro@gmail.com>
2022-04-26 10:10:02 +0200

    Initial commit

---
+++ src/lib/listing.js
@@ -0,0 +1,34 @@
+const products = [
+	{
+		name: 'Chaussure trail',
+		price: 12,
+		quantity: 2
+	},
+	{
+		name: 'Chaussette',
+		price: 12,
+		quantity: 2
+	},
+	{
+		name: 'Chaussure trail',
+		price: 12,
+		quantity: 2
+	},
+	{
+		name: 'Chaussure trail',
+		price: 12,
+		quantity: 2
+	},
+	{
+		name: 'Chaussure trail',
+		price: 12,
+		quantity: 2
+	},
+	{
+		name: 'Chaussure trail',
+		price: 12,
+		quantity: 2
+	}
+];
+
+export default products;
---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+	import products from '$lib/listing.js';
+	let name = 'Romain';
+</script>
+
+
+<section>
+	<p>Bonjour {name}</p>
+	{products}
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
